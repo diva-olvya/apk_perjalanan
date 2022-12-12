2e2b951c6bc19aa86fdd1a96957b9c0e363fb8ce
diva-olvya <divaoliviya2@gmail.com>
2022-12-12 12:41:58 +0700

    first commit

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function index()
+    {
+        $catatan = $this->db->query("SELECT * FROM catatan");
+        $data['catatan'] = $catatan->num_rows();
+        $this->load->view('templates/header');
+        $this->load->view('templates/topbar');
+        $this->load->view('templates/sidebar');
+        $this->load->view('dashboard', $data);
+        $this->load->view('templates/footer');
+    }
+}
